2984509a16b2d8117138239706fbe96910c1602a
Joan Lyness <joanrigdon@gmail.com>
2019-03-21 03:25:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,13 @@
 class Student
-  attr_reader :id
+  attr_reader :db :id
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize(name, DB[:conn])
+  def self.db
+    @db = DB[:conn]
+
+
+  def initialize(name, db)
   end
 
 end
